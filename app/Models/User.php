a4daa1a952dbbfc29dc7dea2346180cfca9e8384
rehanzhaky <hannn9491@gmail.com>
2025-08-17 15:32:40 +0700

    learn: how to implement spatie n breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// Digunakan untuk mengimport Traits
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
+    // import traits tadi digunakan disini
     use HasFactory, Notifiable, HasRoles;
 
     /**
